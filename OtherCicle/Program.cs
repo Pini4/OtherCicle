b6cef17670e15d09349ecccb91c9a29a38b48766
СГК ГАПОУ <sgk.gapou@outlook.com>
2022-09-14 11:18:25 +0400

    Добавьте файлы проекта.

---
+++ OtherCicle/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace OtherCicle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        
+
+            string lang = "ru";
+
+            switch (lang)
+            {
+                case "ru":
+                    Console.WriteLine("Привет мир!");
+                    break;
+                case "en":
+                    Console.WriteLine("Hello world!");
+                    break;
+                default:
+                    Console.WriteLine("Локализация не найдена");
+                    break;
+                case "fr":
+                    Console.WriteLine("Bonjour le monde!");
+                    break;
+                case "jp":
+                    Console.WriteLine("こんにちは。");
+                    break;
+
+            }
+
+
+
+        }
+    }
+}
